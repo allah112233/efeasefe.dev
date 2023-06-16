8916afe55c455e375b17f457ac72054361df3ad3
Efeasefe <123326900+allah112233@users.noreply.github.com>
2023-06-16 18:15:12 +0300

    Update ThemeSwitcher.tsx

--- src/components/Nav/ThemeSwitcher.tsx
+++ src/components/Nav/ThemeSwitcher.tsx
@@ -8,9 +8,9 @@ export const ThemeSwitcher = () => {
 
   const toggle = () => {
     if (theme === 'dark') {
-      setTheme('light')
-    } else {
       setTheme('dark')
+    } else {
+      setTheme('light')
     }
   }
 
